91e12b45aa43118db18d0d7e0a96c903ad33d3bc
Ahmed Ansari <ansari82002@gmail.com>
2024-04-08 13:15:29 +0100

    added files

---
+++ clip_images.py
@@ -0,0 +1,39 @@
+from PIL import Image
+import os
+import pandas as pd
+
+
+
+def clip_image(image_path, xmin, ymin, xmax, ymax, categories):  # Open the image
+    image = Image.open(image_path)
+    cropped_image = image.crop((xmin, ymin, xmax, ymax))
+    file_hash = hash(f"{xmin} {ymin}  {xmax} {ymax}  {categories}")
+    file_path = f"./datasets/v1/validate/{categories}/{file_hash}.jpg"
+    save_path = f"./datasets/v1/validate/{categories}"
+    if os.path.exists(save_path):
+        cropped_image.save(file_path)
+    else:
+        os.makedirs(save_path)
+        cropped_image.save(file_path)
+
+
+if __name__ == "__main__":
+    # CSV file containing coordinates (assuming format: left,upper,right,lower)
+    csv_file = "./roboflow/valid/_annotations.csv"
+    path = os.getcwd()
+    print(path)
+    df = pd.read_csv(os.path.join(path, csv_file))
+    distinct_categories = df['class'].unique()
+    print(distinct_categories)
+    for index, row in df.iterrows():
+        file_name = os.path.join(path, f"./roboflow/valid/{row['filename']}")
+        clip_image(
+            file_name,
+            row["xmin"],
+            row["ymin"],
+            row["xmax"],
+            row["ymax"],
+            row["class"],
+        )
+
+
